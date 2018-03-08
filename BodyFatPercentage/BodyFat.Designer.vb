54d9651be1b46efc5dc8784fff503792d27271f1
KmanT <kturske@gmail.com>
2018-03-08 17:47:14 -0500

    First commit
    first

---
+++ BodyFatPercentage/BodyFat.Designer.vb
@@ -0,0 +1,290 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class BodyFat
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.grpSex = New System.Windows.Forms.GroupBox()
+        Me.rdbFemale = New System.Windows.Forms.RadioButton()
+        Me.rdbMale = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lblHips = New System.Windows.Forms.Label()
+        Me.txtAge = New System.Windows.Forms.TextBox()
+        Me.txtHeight = New System.Windows.Forms.TextBox()
+        Me.txtNeck = New System.Windows.Forms.TextBox()
+        Me.txtWaist = New System.Windows.Forms.TextBox()
+        Me.txtHips = New System.Windows.Forms.TextBox()
+        Me.txtBodyFat = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.grpMeasurements = New System.Windows.Forms.GroupBox()
+        Me.lblEligible = New System.Windows.Forms.Label()
+        Me.grpSex.SuspendLayout()
+        Me.grpMeasurements.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(13, 225)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(97, 42)
+        Me.btnEnter.TabIndex = 4
+        Me.btnEnter.Text = "Calculate"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(127, 225)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(92, 42)
+        Me.btnClear.TabIndex = 5
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(232, 225)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(97, 42)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'grpSex
+        '
+        Me.grpSex.Controls.Add(Me.rdbFemale)
+        Me.grpSex.Controls.Add(Me.rdbMale)
+        Me.grpSex.Location = New System.Drawing.Point(13, 13)
+        Me.grpSex.Name = "grpSex"
+        Me.grpSex.Size = New System.Drawing.Size(96, 86)
+        Me.grpSex.TabIndex = 1
+        Me.grpSex.TabStop = False
+        Me.grpSex.Text = "Sex"
+        '
+        'rdbFemale
+        '
+        Me.rdbFemale.AutoSize = True
+        Me.rdbFemale.Location = New System.Drawing.Point(6, 48)
+        Me.rdbFemale.Name = "rdbFemale"
+        Me.rdbFemale.Size = New System.Drawing.Size(75, 21)
+        Me.rdbFemale.TabIndex = 1
+        Me.rdbFemale.TabStop = True
+        Me.rdbFemale.Text = "Female"
+        Me.rdbFemale.UseVisualStyleBackColor = True
+        '
+        'rdbMale
+        '
+        Me.rdbMale.AutoSize = True
+        Me.rdbMale.Location = New System.Drawing.Point(6, 21)
+        Me.rdbMale.Name = "rdbMale"
+        Me.rdbMale.Size = New System.Drawing.Size(59, 21)
+        Me.rdbMale.TabIndex = 0
+        Me.rdbMale.TabStop = True
+        Me.rdbMale.Text = "Male"
+        Me.rdbMale.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 114)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(33, 17)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Age"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 26)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(104, 17)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Height (Inches)"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 54)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(95, 17)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Neck (Inches)"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(6, 82)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(98, 17)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Waist (Inches)"
+        '
+        'lblHips
+        '
+        Me.lblHips.AutoSize = True
+        Me.lblHips.Location = New System.Drawing.Point(6, 110)
+        Me.lblHips.Name = "lblHips"
+        Me.lblHips.Size = New System.Drawing.Size(91, 17)
+        Me.lblHips.TabIndex = 3
+        Me.lblHips.Text = "Hips (Inches)"
+        Me.lblHips.Visible = False
+        '
+        'txtAge
+        '
+        Me.txtAge.Location = New System.Drawing.Point(68, 111)
+        Me.txtAge.MaxLength = 3
+        Me.txtAge.Name = "txtAge"
+        Me.txtAge.Size = New System.Drawing.Size(41, 22)
+        Me.txtAge.TabIndex = 2
+        '
+        'txtHeight
+        '
+        Me.txtHeight.Location = New System.Drawing.Point(116, 23)
+        Me.txtHeight.MaxLength = 4
+        Me.txtHeight.Name = "txtHeight"
+        Me.txtHeight.Size = New System.Drawing.Size(62, 22)
+        Me.txtHeight.TabIndex = 0
+        '
+        'txtNeck
+        '
+        Me.txtNeck.Location = New System.Drawing.Point(116, 51)
+        Me.txtNeck.MaxLength = 4
+        Me.txtNeck.Name = "txtNeck"
+        Me.txtNeck.Size = New System.Drawing.Size(62, 22)
+        Me.txtNeck.TabIndex = 1
+        '
+        'txtWaist
+        '
+        Me.txtWaist.Location = New System.Drawing.Point(116, 79)
+        Me.txtWaist.MaxLength = 4
+        Me.txtWaist.Name = "txtWaist"
+        Me.txtWaist.Size = New System.Drawing.Size(62, 22)
+        Me.txtWaist.TabIndex = 2
+        '
+        'txtHips
+        '
+        Me.txtHips.Location = New System.Drawing.Point(116, 107)
+        Me.txtHips.MaxLength = 4
+        Me.txtHips.Name = "txtHips"
+        Me.txtHips.Size = New System.Drawing.Size(62, 22)
+        Me.txtHips.TabIndex = 3
+        Me.txtHips.Visible = False
+        '
+        'txtBodyFat
+        '
+        Me.txtBodyFat.Location = New System.Drawing.Point(76, 177)
+        Me.txtBodyFat.MaxLength = 10
+        Me.txtBodyFat.Name = "txtBodyFat"
+        Me.txtBodyFat.ReadOnly = True
+        Me.txtBodyFat.Size = New System.Drawing.Size(56, 22)
+        Me.txtBodyFat.TabIndex = 14
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(10, 180)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(60, 17)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "Bodyfat "
+        '
+        'grpMeasurements
+        '
+        Me.grpMeasurements.Controls.Add(Me.Label2)
+        Me.grpMeasurements.Controls.Add(Me.Label3)
+        Me.grpMeasurements.Controls.Add(Me.txtHips)
+        Me.grpMeasurements.Controls.Add(Me.Label4)
+        Me.grpMeasurements.Controls.Add(Me.txtWaist)
+        Me.grpMeasurements.Controls.Add(Me.lblHips)
+        Me.grpMeasurements.Controls.Add(Me.txtNeck)
+        Me.grpMeasurements.Controls.Add(Me.txtHeight)
+        Me.grpMeasurements.Location = New System.Drawing.Point(139, 13)
+        Me.grpMeasurements.Name = "grpMeasurements"
+        Me.grpMeasurements.Size = New System.Drawing.Size(190, 143)
+        Me.grpMeasurements.TabIndex = 3
+        Me.grpMeasurements.TabStop = False
+        Me.grpMeasurements.Text = "Measurements"
+        '
+        'lblEligible
+        '
+        Me.lblEligible.AutoSize = True
+        Me.lblEligible.Location = New System.Drawing.Point(157, 180)
+        Me.lblEligible.Name = "lblEligible"
+        Me.lblEligible.Size = New System.Drawing.Size(62, 17)
+        Me.lblEligible.TabIndex = 17
+        Me.lblEligible.Text = "Eligibility"
+        Me.lblEligible.Visible = False
+        '
+        'BodyFat
+        '
+        Me.AcceptButton = Me.btnEnter
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnExit
+        Me.ClientSize = New System.Drawing.Size(342, 278)
+        Me.Controls.Add(Me.lblEligible)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.grpMeasurements)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtBodyFat)
+        Me.Controls.Add(Me.grpSex)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnEnter)
+        Me.Controls.Add(Me.txtAge)
+        Me.Name = "BodyFat"
+        Me.Text = "Body Fat Percentage Calculatator"
+        Me.grpSex.ResumeLayout(False)
+        Me.grpSex.PerformLayout()
+        Me.grpMeasurements.ResumeLayout(False)
+        Me.grpMeasurements.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents grpSex As GroupBox
+    Friend WithEvents rdbFemale As RadioButton
+    Friend WithEvents rdbMale As RadioButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lblHips As Label
+    Friend WithEvents txtAge As TextBox
+    Friend WithEvents txtHeight As TextBox
+    Friend WithEvents txtNeck As TextBox
+    Friend WithEvents txtWaist As TextBox
+    Friend WithEvents txtHips As TextBox
+    Friend WithEvents txtBodyFat As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents grpMeasurements As GroupBox
+    Friend WithEvents lblEligible As Label
+End Class
---
+++ BodyFatPercentage/BodyFat.vb
@@ -0,0 +1,253 @@
+﻿'Kyle Turske
+'Military Body Fat Calculator
+'10/3/2016
+Public Class BodyFat
+
+    Const AGE_MIN As Integer = 17
+    Const MEASURE_MIN As Double = 1
+    Const AGE_MAX As Integer = 68
+    Const HEIGHT_MAX As Double = 85
+    Const NECK_MAX As Double = 25
+    Const WAIST_MAX As Double = 60
+    Const HIP_MAX As Double = 54
+
+    Private Function IsPresent(textBox As TextBox, name As String) As Boolean
+        If textBox.Text = "" Then
+            MessageBox.Show(name & " is a required field.", "Entry Error")
+            textBox.Select()
+            Return False
+        Else
+            Return True
+        End If
+    End Function
+
+    'You may also need to add other type check functions – such as IsDouble, IsInteger, etc.
+    Private Function IsDouble(textBox As TextBox, name As String) As Boolean
+        Dim number As Double = 0
+        If Double.TryParse(textBox.Text, number) Then
+            Return True
+        Else
+            MessageBox.Show(name & " must be a decimal value.", "Entry Error")
+            textBox.Select()
+            textBox.SelectAll()
+            Return False
+        End If
+    End Function
+
+    Private Function IsInt(textBox As TextBox, name As String) As Boolean
+        Dim number As Integer = 0
+        If Integer.TryParse(textBox.Text, number) Then
+            Return True
+        Else
+            MessageBox.Show(name & " must be a integer value.", "Entry Error")
+            textBox.Select()
+            textBox.SelectAll()
+            Return False
+        End If
+    End Function
+
+    Private Function IsWithinRange(textBox As TextBox, name As String, min As Double,
+      max As Double) As Boolean
+        Dim number As Double = CDec(textBox.Text)
+        If number < min OrElse number > max Then
+            MessageBox.Show(name & " must be between " & min & " and " & max & ".", "Entry Error")
+            textBox.Select(0, textBox.Text.Length)
+            Return False
+        Else
+            Return True
+        End If
+    End Function
+
+    Private Function IsValid(textBox As TextBox, name As String, min As Integer, max As Integer) As Boolean
+        If IsPresent(textBox, name) = False Then
+            Return False
+        ElseIf IsInt(textBox, name) = False Then
+            Return False
+        ElseIf IsWithinRange(textBox, name, min, max) = False Then
+            Return False
+        Else
+            Return True
+        End If
+    End Function
+
+    Private Function IsValid(textBox As TextBox, name As String, min As Double, max As Double) As Boolean
+        If IsPresent(textBox, name) = False Then
+            Return False
+        ElseIf IsDouble(textBox, name) = False Then
+            Return False
+        ElseIf IsWithinRange(textBox, name, min, max) = False Then
+            Return False
+        Else
+            Return True
+        End If
+    End Function
+
+    Private Function CalcBodyFat(height As Double, neck As Double, waist As Double) As Double 'Male Function
+        Dim answer As Double
+        answer = 86.01 * Math.Log10(waist - neck) - 70.041 * Math.Log10(height) + 36.76
+        Return answer
+    End Function
+
+    Private Function CalcBodyFat(height As Double, neck As Double, waist As Double, hip As Double) As Double 'Female Function
+        Dim answer As Double
+        answer = 163.205 * Math.Log10(waist + hip - neck) - 97.684 * Math.Log10(height) - 78.387
+        Return answer
+    End Function
+
+    Private Function IsEligible(gender As Boolean, age As Integer, bFat As Double) As Boolean
+        'True = Male False = Female
+        Dim check As Boolean
+
+        If gender = True Then 'Male
+            Select Case age
+                Case 17 To 20
+                    If bFat > 20 Then
+                        check = False
+                    Else
+                        check = True
+                    End If
+                Case 21 To 27
+                    If bFat > 22 Then
+                        check = False
+                    Else
+                        check = True
+                    End If
+                Case 28 To 39
+                    If bFat > 24 Then
+                        check = False
+                    Else
+                        check = True
+                    End If
+                Case > 40
+                    If bFat > 26 Then
+                        check = False
+                    Else
+                        check = True
+                    End If
+            End Select
+        ElseIf gender = False Then 'Female
+            Select Case age
+                Case 17 To 20
+                    If bFat > 28 Then
+                        check = False
+                    Else
+                        check = True
+                    End If
+                Case 21 To 27
+                    If bFat > 30 Then
+                        check = False
+                    Else
+                        check = True
+                    End If
+                Case 28 To 39
+                    If bFat > 32 Then
+                        check = False
+                    Else
+                        check = True
+                    End If
+                Case > 40
+                    If bFat > 34 Then
+                        check = False
+                    Else
+                        check = True
+                    End If
+            End Select
+        End If
+        Return check
+    End Function
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+
+        Dim Age As Integer
+        Dim BodyFat As Double
+        Dim IsMale As Boolean
+
+        If rdbMale.Checked Then
+            IsMale = True
+            If IsValid(txtAge, txtAge.Name, AGE_MIN, AGE_MAX) = False Then
+                txtAge.Focus()
+            ElseIf IsValid(txtHeight, txtHeight.Name, MEASURE_MIN, HEIGHT_MAX) = False Then
+                txtHeight.Focus()
+            ElseIf IsValid(txtNeck, txtNeck.Name, MEASURE_MIN, NECK_MAX) = False Then
+                txtNeck.Focus()
+            ElseIf IsValid(txtWaist, txtWaist.Name, MEASURE_MIN, WAIST_MAX) = False Then
+                txtWaist.Focus()
+            Else
+                Age = CInt(txtAge.Text)
+                'Calls Female Function
+                BodyFat = CalcBodyFat(CDbl(txtHeight.Text), CDbl(txtNeck.Text), CDbl(txtWaist.Text))
+
+                If IsEligible(IsMale, Age, BodyFat) = True Then
+                    lblEligible.Text = "You are eligible."
+                Else
+                    lblEligible.Text = "You are ineligible."
+                End If
+
+                lblEligible.Visible = True
+                BodyFat = Math.Round(BodyFat, 2)
+                txtBodyFat.Text = CStr(BodyFat) & "%"
+
+            End If 'End Male Logic
+
+        ElseIf rdbFemale.Checked Then
+            IsMale = False
+            If IsValid(txtAge, txtAge.Name, AGE_MIN, AGE_MAX) = False Then
+                txtAge.Focus()
+            ElseIf IsValid(txtHeight, txtHeight.Name, MEASURE_MIN, HEIGHT_MAX) = False Then
+                txtHeight.Focus()
+            ElseIf IsValid(txtNeck, txtNeck.Name, MEASURE_MIN, NECK_MAX) = False Then
+                txtNeck.Focus()
+            ElseIf IsValid(txtWaist, txtWaist.Name, MEASURE_MIN, WAIST_MAX) = False Then
+                txtWaist.Focus()
+            ElseIf IsValid(txtHips, txtHips.Name, MEASURE_MIN, HIP_MAX) = False Then
+                txtHips.Focus()
+
+            Else
+                Age = CInt(txtAge.Text)
+                'Calls Female Function
+                BodyFat = CalcBodyFat(CDbl(txtHeight.Text), CDbl(txtNeck.Text), CDbl(txtWaist.Text), CDbl(txtHips.Text))
+
+                If IsEligible(IsMale, Age, BodyFat) = True Then
+                    lblEligible.Text = "You are eligible."
+                Else
+                    lblEligible.Text = "You are ineligible."
+                End If
+
+                lblEligible.Visible = True
+                BodyFat = Math.Round(BodyFat, 2)
+                txtBodyFat.Text = CStr(BodyFat) & " %"
+
+            End If 'End Female Logic
+        Else
+            MessageBox.Show("You must enter a gender.", "Entry Error")
+            rdbMale.Focus()
+        End If
+    End Sub
+
+    Private Sub rdbFemale_CheckedChanged(sender As Object, e As EventArgs) Handles rdbFemale.CheckedChanged
+        lblHips.Visible = True
+        txtHips.Visible = True
+
+    End Sub
+
+    Private Sub rdbMale_CheckedChanged(sender As Object, e As EventArgs) Handles rdbMale.CheckedChanged
+        lblHips.Visible = False
+        txtHips.Visible = False
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        rdbMale.Checked = False
+        rdbFemale.Checked = False
+        txtAge.Text = ""
+        txtBodyFat.Text = ""
+        txtHeight.Text = ""
+        txtHips.Text = ""
+        txtNeck.Text = ""
+        txtWaist.Text = ""
+        lblEligible.Visible = False
+    End Sub
+End Class
